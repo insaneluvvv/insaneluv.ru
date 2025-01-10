8378b0a13558bebb3bee554cef04eeada81950af
tyomzya <tyomzya@mail.ru>
2025-01-10 15:40:45 +0300

    Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,13 +16,22 @@ const HeaderWrapper = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 50px;
+  height: 40px;
   background-color: #21A049;
+  transition: 300ms ease;
   span {
     color: #202020;
     font-family: "Unbounded", serif;
-    font-size: 30px;
+    font-size: 25px;
     font-weight: 600;
+    transition: 500ms ease;
+  }
+  
+  &:hover {
+    background-color: #1b7e39;
+    span {
+      font-size: 28px;
+    }
   }
 `;
 
