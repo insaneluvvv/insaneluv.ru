39b35173812842c319278faab86651c4221462c8
tyomzya <tyomzya@mail.ru>
2025-01-10 15:40:55 +0300

    Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,10 @@ import { ImMail4 } from "react-icons/im";
 const Footer = () => {
   return ( 
     <FooterWrapper>
-      <span>Сайт резюме-портфолио Лемешева Артема: Front-End разработка</span>
+      <DownloadCVButton href="./src/assets/media/Avatar.png" download="LemeshevArtem_CV.png">
+        Скачать CV
+      </DownloadCVButton>
+      <span>Лемешев Артем // FrontEnd разработка</span>
       <Socials>
         <FaTelegram size={30}/>
         <FaGithub size={30}/>
@@ -25,6 +28,28 @@ const FooterWrapper = styled.div`
   background-color: #303030;
   color: #909090;
   padding: 0 5%;
+  @media (max-width: 800px) {
+    flex-direction: column;
+    padding: 20px 5%;
+  }
+`;
+
+const DownloadCVButton = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  width: 200px;
+  background-color: #909090;
+  border: 0;
+  border-radius: 15px;
+  font-size: 18px;
+  font-weight: 600;
+  color: #303030;
+  transition: 300ms ease;
+  &:hover{
+    background-color: #707070;
+  }
 `;
 
 const Socials = styled.div`
@@ -32,5 +57,7 @@ const Socials = styled.div`
   justify-content: space-between;
   width: 110px;
 `;
+
+
 
 export default Footer;
