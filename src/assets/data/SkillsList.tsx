ce7b7d2ac7c40fe98a126d942c82c7833729099f
tyomzya <tyomzya@mail.ru>
2025-01-14 18:20:29 +0300

    Added additional skills

---
+++ src/assets/data/SkillsList.tsx
@@ -0,0 +1,23 @@
+export const DevSkillsFirst : Array<string> = [
+  `React`,
+  `TypeScript`,
+  `HTML`,
+  `CSS`,
+  `Git`,
+  `Vite`
+]
+
+export const DevSkillsSecond : Array<string> = [
+  `React Router Dom`,
+  `Styled Component`,
+  `Docker`,
+  `Адаптивная верстка`
+]
+
+export const AdditionalSkills : Array<string> = [
+  `Figma`,
+  `Adobe Photoshop`,
+  `Adobe Illustrator`,
+  `Администрирование сервера Ubuntu`,
+  `Администрирование домена`
+]
